3b726a76dad50580ef6fa0ba0b37f7f72cecbf2e
Enoch2k2 <enoch2k2@hotmail.com>
2022-09-23 09:07:42 -0500

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [added, setAdded] = useState(false);
+
+  const displayButtonText = () => added ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={added ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={ () => setAdded(!added) }>{ displayButtonText() }</button>
     </li>
   );
 }
